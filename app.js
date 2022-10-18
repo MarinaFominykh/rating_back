a0b2e52692b4a9b0670437724f205bbc57dfda89
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2022-10-18 21:39:25 +0300

    логирование отключено

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ mongoose.connect('mongodb://localhost:27017/ratingdb', {
 const matchRouter = require('./routes/matches');
 const unitRouter = require('./routes/units');
 const handlerErrors = require('./middlewares/handleErrors');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-app.use(requestLogger);
+// app.use(requestLogger);
 app.use(cors());
 app.use(matchRouter);
 app.use(unitRouter);
-app.use(errorLogger);
+// app.use(errorLogger);
 app.use(handlerErrors);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
